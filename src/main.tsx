af830016e111253c074883a5d50a892d54cfcf18
Savilus <jakublanoszka@gmail.com>
2024-06-30 20:36:02 +0200

    configuration
    talwind material
    react routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import './app/index.css';
+import {ThemeProvider} from '@material-tailwind/react';
+import App from './App.tsx';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import * as ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <ThemeProvider>
+            <BrowserRouter basename="/budget-buddy">
+                <Routes>
+                    <Route path="/*" element={<App/>}/>
+                </Routes>
+            </BrowserRouter>
+        </ThemeProvider>
+    </React.StrictMode>
+);
